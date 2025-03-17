3d52934749ec900c4c8309bc5faeddf913b0de83
youngmi <wjdym14@gmail.com>
2025-03-17 18:30:04 +0900

    feat(component): table

---
+++ lib/components/molecules/Table.tsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import {
+	Table as MuiTable,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Paper,
+	TablePagination,
+	styled,
+} from '@mui/material';
+import {
+	useReactTable,
+	getCoreRowModel,
+	flexRender,
+	type ColumnDef,
+	getPaginationRowModel,
+} from '@tanstack/react-table';
+import { bds } from '@/constants';
+
+interface Props<TData> {
+	data: TData[];
+	columns: ColumnDef<TData>[];
+}
+
+export const Table = <TData,>({ data, columns }: Props<TData>) => {
+	const [pageIndex, setPageIndex] = useState(0);
+	const [pageSize, setPageSize] = useState(5); // 기본 페이지 크기
+
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		state: {
+			pagination: { pageIndex, pageSize },
+		},
+		onPaginationChange: (updater) => {
+			const newPagination = typeof updater === 'function' ? updater({ pageIndex, pageSize }) : updater;
+			setPageIndex(newPagination.pageIndex);
+			setPageSize(newPagination.pageSize);
+		},
+	});
+
+	return (
+		<TableContainer component={Paper}>
+			<MuiTable>
+				<TableHead>
+					<TableRow>
+						{table
+							.getHeaderGroups()
+							.map((headerGroup) =>
+								headerGroup.headers.map((header) => (
+									<S.TableCell key={header.id}>
+										{flexRender(header.column.columnDef.header, header.getContext())}
+									</S.TableCell>
+								)),
+							)}
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{table.getRowModel().rows.map((row) => (
+						<TableRow
+							key={row.id}
+							sx={{
+								'&:hover': {
+									backgroundColor: bds.token.color.gray.gray005,
+								},
+							}}
+						>
+							{row.getVisibleCells().map((cell) => (
+								<TableCell key={cell.id}>
+									{flexRender(cell.column.columnDef.cell, cell.getContext())}
+								</TableCell>
+							))}
+						</TableRow>
+					))}
+				</TableBody>
+			</MuiTable>
+			<TablePagination
+				component="div"
+				count={data.length}
+				page={pageIndex}
+				rowsPerPage={pageSize}
+				onPageChange={(_, newPage) => setPageIndex(newPage)}
+				onRowsPerPageChange={(event) => setPageSize(Number(event.target.value))}
+			/>
+		</TableContainer>
+	);
+};
+
+const S = {
+	TableCell: styled(TableCell)({
+		fontWeight: 'bold',
+		backgroundColor: bds.token.color.gray.gray005,
+	}),
+};
