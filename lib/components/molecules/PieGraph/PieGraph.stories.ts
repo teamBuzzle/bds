931fab4c3c2c91780f618a18e13baeeebb0d8040
youngmi <wjdym14@gmail.com>
2025-03-29 22:36:11 +0900

    feat(graph): pie graph

---
+++ lib/components/molecules/PieGraph/PieGraph.stories.ts
@@ -0,0 +1,41 @@
+import { PieGraph } from './PieGraph';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof PieGraph> = {
+	title: 'Molecules/PieGraph',
+	component: PieGraph,
+	parameters: {
+		layout: 'centered',
+		backgrounds: {
+			default: 'dark',
+		},
+	},
+	argTypes: {
+		data: {
+			description: '그래프 데이터',
+			control: 'object',
+		},
+		legends: {
+			description: 'legends 여부',
+			control: 'boolean',
+		},
+	},
+	tags: ['autodocs'],
+} satisfies Meta<typeof PieGraph>;
+
+export default meta;
+type Story = StoryObj<typeof PieGraph>;
+
+const data = [
+	{ label: '라벨1', value: 40, color: '#ff6384' },
+	{ label: '라벨2', value: 30, color: '#36a2eb' },
+	{ label: '라벨3', value: 20, color: '#ffcd56' },
+	{ label: '라벨4', value: 80, color: '#4bc0c0' },
+];
+
+export const Default: Story = {
+	args: {
+		data,
+		legends: true,
+	},
+};
