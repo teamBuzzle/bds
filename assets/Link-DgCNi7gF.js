90d7299d7eacdf2960c3121bd2dc16b395587974
devxian96 <devxian96@users.noreply.github.com>
2025-04-28 15:27:24 +0000

    deploy: 7d0575b6fad06c559de6e09d7f1e58e3ecd8bd1e

---
+++ assets/Link-DgCNi7gF.js
@@ -0,0 +1,2 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{L as i}from"./Link-vUMxFn4P.js";const e=({href:r,cta:o,children:s,...t})=>a.jsx(i,{href:r,...t,className:o?"buzzle-cta":"",children:s});try{e.displayName="Link",e.__docgenInfo={description:"링크 컴포넌트",displayName:"Link",props:{}}}catch{}export{e as L};
+//# sourceMappingURL=Link-DgCNi7gF.js.map
